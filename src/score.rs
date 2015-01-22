b76861c0cbf801a4a4fd457c48c01bce095c26a4
Herman J. Radtke III <hermanradtke@gmail.com>
2015-01-21 19:45:00 -0800

    get some

--- src/score.rs
+++ src/score.rs
@@ -10,41 +10,35 @@ pub fn score(choice: &str, query: &str) -> f64 {
         return 0.0;
     }
 
-    //let lower_choice = choice.into_ascii_lowercase();
     let lower_choice = choice.to_ascii_lowercase();
     let lower_query = query.to_ascii_lowercase();
     let lower_choice_len = lower_choice.len() as f64;
 
-    //println!("{} in {}", lower_query, lower_choice);
-    let match_length = compute_match_length(lower_choice, lower_query.chars().collect());
+    let match_length = compute_match_length(lower_choice.as_slice(), lower_query.chars().collect());
 
     if match_length == 0 {
         return 0.0;
     }
 
-    //println!("{} / {}", lower_query.len(), match_length);
     let score = lower_query.len() as f64 / match_length as f64;
 
-    //println!("{} / {}", score, lower_choice_len);
     score / lower_choice_len
 }
 
 /// Find the length of the shortest substring matching the given characters.
-fn compute_match_length(haystack: String, needles: Vec<char>) -> usize {
+fn compute_match_length(haystack: &str, needles: Vec<char>) -> usize {
     let first_char = needles[0];
     let rest = needles.slice_from(1);
     let mut min_index = 0;
 
-    let first_indexes = find_char_in_string(haystack.as_slice(), first_char);
+    let first_indexes = find_char_in_string(haystack, first_char);
 
     for first_index in first_indexes.iter() {
-        //println!("first_index = {}", first_index);
-        let last_index = find_end_of_match(haystack.as_slice(), rest, *first_index);
+        let last_index = find_end_of_match(haystack, rest, *first_index);
 
         match last_index {
             Some(index) => {
                 let new_index = index - *first_index + 1;
-                //println!("last_index = {}, new_index = {}, min_index = {}", index, new_index, min_index);
                 if min_index == 0 || new_index < min_index {
                     min_index = new_index;
                 }
@@ -53,7 +47,6 @@ fn compute_match_length(haystack: String, needles: Vec<char>) -> usize {
         }
     }
 
-    //println!("min_index = {}", min_index);
     min_index
 }
 
@@ -61,11 +54,9 @@ fn compute_match_length(haystack: String, needles: Vec<char>) -> usize {
 fn find_char_in_string(haystack: &str, needle: char) -> Vec<usize> {
     let mut index: usize = 0;
     let mut indexes = Vec::new();
-    let mut h = haystack.as_slice();
 
-    //println!("{}", "find_char_in_string");
     loop {
-        index = match find_from_offset(h, needle, index) {
+        index = match find_from_offset(haystack, needle, index) {
             Some(i) => {
                 indexes.push(i);
                 i + 1
@@ -79,9 +70,7 @@ fn find_char_in_string(haystack: &str, needle: char) -> Vec<usize> {
 
 /// Find each of the characters in the string, moving strictly left to right.
 fn find_end_of_match(haystack: &str, needles: &[char], first_index: usize) -> Option<usize> {
-    //println!("at this point haystack = {}", haystack);
     let mut last_index = first_index;
-    //println!("{}", "find_end_of_match");
     for needle in needles.iter() {
         last_index = match find_from_offset(haystack, *needle, last_index + 1) {
             Some(i) => i,
@@ -95,7 +84,6 @@ fn find_end_of_match(haystack: &str, needles: &[char], first_index: usize) -> Op
 /// Implements Ruby's `#index` method
 fn find_from_offset(haystack: &str, needle: char, offset: usize) -> Option<usize> {
     let h = haystack.slice_from(offset);
-    //println!("from_from_offset: h = {}, needle = {}, offset = {}", h, needle, offset);
 
     let index = h.find(needle);
 
